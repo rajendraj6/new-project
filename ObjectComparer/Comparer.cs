1d7d3ec56dedbfd9b800f2f6f7f59c70bf8ad42e
rajendraj6 <rajendraj6@gmail.com>
2020-04-03 12:26:59 +0530

    object compare

---
+++ ObjectComparer/Comparer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ObjectComparer
+{
+    public static class Comparer
+    {
+        public static bool AreSimilar<T>(T first, T second )
+        {
+            if(first.SerializeObject() == second.SerializeObject())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            /// Add your implementation logic here. Feel free to add classes and types as required for your solution.
+           // return true;
+        }
+        public static string SerializeObject<T>(this T toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+
+}
